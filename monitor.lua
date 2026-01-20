cb88b423e85c89fabbecf9b9a4c6f27840587f11
yudhagans2006 <yudhagans2006@gmail.com>
2026-01-20 08:50:55 +0700

    Updatenew monitor.lua

--- monitor.lua
+++ monitor.lua
@@ -1,33 +1,25 @@
--- Monitor to Web - Kirim data ke endpoint gratis
 local HttpService = game:GetService("HttpService")
 local Players = game:GetService("Players")
 
-local ENDPOINT = "https://api.jsonbin.io/v3/b/[BIN_ID]?meta=false"  -- Nanti ganti
-local SECRET_KEY = "$2b$10$[HASH]"  -- Nanti
-
-local function getFishData()
-    local data = {executor = Players.LocalPlayer.Name, timestamp = os.time(), players = {}}
-    for _, p in Players:GetPlayers() do
-        data.players[p.Name] = {
-            Common = math.random(10,50), -- Ganti real inventory
-            Uncommon = math.random(5,20),
-            Rare = math.random(0,10),
-            Epic = math.random(0,5),
-            Legendary = math.random(0,2)
-        }
-    end
-    return HttpService:JSONEncode(data)
-end
+-- PASTE URL webhook.site KAMU DISINI
+local url = "https://webhook.site/56cec7a6-3a50-45b8-8438-275d94b9be2b"
 
 spawn(function()
-    while true do
-        HttpService:RequestAsync({
-            Url = ENDPOINT,
-            Method = "PUT",
-            Headers = {["Content-Type"] = "application/json", ["X-Master-Key"] = SECRET_KEY},
-            Body = getFishData()
-        })
-        wait(15)
+    while wait(15) do
+        local data = {
+            executor = Players.LocalPlayer.Name,
+            players = {}
+        }
+        for _,p in Players:GetPlayers() do
+            data.players[p.Name] = {
+                Common=math.random(10,50),
+                Uncommon=math.random(5,25),
+                Rare=math.random(1,10),
+                Epic=math.random(0,5),
+                Legendary=math.random(0,2)
+            }
+        end
+        HttpService:PostAsync(url, HttpService:JSONEncode(data))
     end
 end)
-print("Data sent to web monitor!")
+print("Live to webhook.site!")
